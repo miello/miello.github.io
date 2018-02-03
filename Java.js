0285063303487c6219a009bf990a55ee18f7c142
miello <35169079+miello@users.noreply.github.com>
2018-02-03 15:28:42 +0700

    Update Java.js

--- Java.js
+++ Java.js
@@ -1,6 +1,10 @@
-
 // JavaScript Document
 var test1 = 0;
 function enablevideo(){
   document.getElementById("vid").hidden="";
+  document.getElementById("tp").hidden="hidden";
+}
+
+function load(){
+  document.getElementById("vid").hidden="hidden";
 }
