52417fdcca49f2d41400b475f9f5dde89dabba8c
miello <35169079+miello@users.noreply.github.com>
2018-02-04 23:06:54 +0700

    Update Java.js

--- Java.js
+++ Java.js
@@ -21,3 +21,27 @@ function load(){
   document.getElementById("head").hidden="";
 }
 
+window.onload = function() {
+    document.addEventListener("contextmenu", function(e){
+      e.preventDefault();
+    }, false);
+    document.addEventListener("keydown", function(e) {
+    //document.onkeydown = function(e) {
+      // "F12" key
+      if (event.keyCode == 123) {
+        disabledEvent(e);
+      }
+    }, false);
+    function disabledEvent(e){
+      if (e.stopPropagation){
+        e.stopPropagation();
+      } else if (window.event){
+        window.event.cancelBubble = true;
+      }
+      e.preventDefault();
+      return false;
+    }
+  };
+
+
+
